d9d467767be7f0def3609361401740109c9f7384
ArturZelenec <82833670+ArturZelenec@users.noreply.github.com>
2022-01-26 14:34:49 +0200

    Add project files.

---
+++ ConsoleAppTryCatch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Wrong format");
+            }
+        }
+    }
+}
